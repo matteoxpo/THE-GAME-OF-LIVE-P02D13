f799ccd163a83a89168b1e5f3e8441d508bd7a63
Kwaltz Buck <kwaltzbu@ex-k3.nsk.21-school.ru>
2022-08-06 21:25:59 +0700

    saves

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -4,13 +4,16 @@
 #include <time.h>
 #include <unistd.h>
 
-#define N 5
-#define M 5
+#define N 25
+#define M 80
 #define LIFE "*"
-#define DEAD "-"
+#define DEAD " "
 
-// void gameMenu();
-void game();
+void gameMenu();
+void printMenuOptions();
+void printAboutGame();
+void saveScan(int *command);
+void game(int mode);
 int allocMemory(char ***matrix);
 void freeMemory(char **matrix);
 void generation(char **matrix);
@@ -22,36 +25,75 @@ int countAliveCells(char **matrix, int i, int j);
 int sameMatrix(char **matrix, char **buff);
 
 int main() {
-  game();
+  game(1);
   return 0;
 }
 
-void game() {
+void gameMenu() {
+  int check = 0;
+  int command;
+  while (1) {
+    printMenuOptions();
+    saveScan(&command);
+    switch (command) {
+      case 1:
+        break;
+      case 2:
+        break;
+    }
+  }
+}
+void printMenuOptions() {
+  printf("1 - пользовательский ввод игры\n");
+  printf("2 - запустить карту gunGospy\n");
+  printf("3 - запустить карту gunSim\n");
+  printf("4 - запустить карту agar\n");
+}
+
+void printAboutGame() {
+  printf("Игра в жизнь\n");
+  printf(
+      "Место действия игры — размеченная на клетки плоскость, которая может "
+      "быть безграничной, ограниченной, или замкнутой.\n");
+  printf("В нашем случае поле %d на %d размером и сфеерически замкнута.\n", N,
+         M);
+  printf(
+      "Каждая клетка на этой поверхности имеет восемь соседей, окружающих "
+      "её, "
+      "и может находиться в двух состояниях: быть «живой» (заполненной) или "
+      "«мёртвой» (пустой).\n");
+  printf("Игра прекращается, если\n");
+  printf("* на поле не останется ни одной «живой» клетки\n");
+  printf("* при очередном шаге ни одна из клеток не меняет своего состояния\n");
+}
+void saveScan(int *command) {
+  int check = 0;
+  while (!check) {
+    printf("Ввод команды:");
+    check = scanf("%d", command);
+    if (!check) {
+      printf("Ошибка ввода, попробуем снова.\n");
+    }
+  }
+}
+
+void game(int mode) {
   char **matrix;
   char **buff;
   allocMemory(&matrix);
   allocMemory(&buff);
   generation(matrix);
+  fieldOutput(matrix);
   printf("\n");
   while (fieldUpdate(&matrix, &buff)) {
-    // int x;
-    // scanf("%d", &x);
-    printf("\n-------------------------------------\n");
-    sleep(5);
+    // break;
+    // printf("\n000000000000\n");
     fieldOutput(matrix);
+    usleep(1000000);
+    printf("\n");
+    printf("\033[0d\033[2J");
   }
 
-  /*
-    generation(matrix);
-    fieldOutput(matrix);
-    printf("\n---------------------------\n");
-    fieldUpdate(&matrix, &buff);
-    fieldOutput(matrix);
-    printf("\n---------------------------\n");
-    fieldUpdate(&matrix, &buff);
-    fieldOutput(matrix);
-    printf("\n---------------------------\n");
-    */
   freeMemory(matrix);
   freeMemory(buff);
 }
@@ -80,11 +122,12 @@ void freeMemory(char **matrix) {
 void generation(char **matrix) {
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < M; j++) {
-      int x = i % 2;
-      matrix[i][j] = x + '0';
-      printf("%c", matrix[i][j]);
+      char c;
+      scanf(" %c", &c);
+      if (c == '-') c = '0';
+      if (c == 'o') c = '1';
+      matrix[i][j] = c;
     }
-    printf("\n");
   }
 }
 
@@ -147,12 +190,4 @@ void fieldOutput(char **matrix) {
     }
     if (i != N - 1) printf("\n");
   }
-}
-
-int sameMatrix(char **matrix, char **buff) {
-  int check = 1;
-  for (int i = 0; i < N; i++)
-    for (int j = 0; j < M; j++)
-      if (matrix[i][j] != buff[i][j]) check = 0;
-  return check;
 }
